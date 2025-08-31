f928b49963aa250d38ace79f0435f4948803623a
Sangram Poudel <sangrampoudel321@gmail.com>
2025-08-31 09:06:14 +0545

    Add files via upload

---
+++ download_nltk.py
@@ -0,0 +1,11 @@
+# In download_nltk.py
+import nltk
+
+print("Downloading required NLTK data packages...")
+
+nltk.download('stopwords')
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('punkt_tab')
+
+print("Downloads complete.")
